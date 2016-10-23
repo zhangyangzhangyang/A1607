39dd10d268f2b1aca4de71eb217de67a1de1c5ea
张样 <1343253269@qq.com>
2016-10-23 20:41:52 +0800

    二狐游戏项目

--- mytest/src/main/java/com/example/mytest/MainActivity.java
+++ mytest/src/main/java/com/example/mytest/MainActivity.java
@@ -16,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.main_id_txt);//
-        AsyncTaskTool.load("http://big.pipaw.com/api/activities/userindex?token=cnNCa5mMjTvXD%252Bw1YEZagvo%252BpKuh5M2aAl%252FOs29Raz95MnMqCjom4qSWYC%252BSFrelkEVyzVO9u9Jm1RWc7P3YWvJwn2H7qR4I&app_version=343&page_index=1&page_size=10")
+        AsyncTaskTool.load("http://big.pipaw.com/apinew/newgift/Hot")
+                .post("token=cnNCa5mMjTvXD%2Bw1YEZagvo%2BpKuh5M2aAl%2FOs29Raz95MnMqCjom4qSWYC%2BSFrelkEVyzVO9u9Jm1RWc7P3YWvJwn2H7qR4I&app_version=133524429196107")
                .execute(new AsyncTaskTool.IMyCallback() {
                     @Override
                     public void success(String result) {
